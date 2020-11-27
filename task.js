25dc3e4267309d354d5315681090a09a3f66db82
takahumi0806 <yabuta0439@hotmail.com>
2020-11-27 22:28:39 +0900

    Update task.js

--- task.js
+++ task.js
@@ -1,64 +1,64 @@
-window.onload=function() {
+window.onload = function() {
 
-  const tasks=[]
-  const createButton=document.getElementById('task_button');
+  const tasks = [];
+  const taskBtn = document.getElementById('task_button');
 
   function createStatus(newRow, status){
-    newCell=newRow.insertCell();
-    newText=document.createTextNode(status);
+    newCell = newRow.insertCell();
+    newText = document.createTextNode(status);
     newCell.appendChild(newText);
   }
 
   function deleteBtn(newRow, status, id){
-    newCell=newRow.insertCell();
-    newText=document.createElement('button');
-    newText.textContent = status
+    newCell = newRow.insertCell();
+    newText = document.createElement('button');
+    newText.textContent = status;
     newCell.appendChild(newText);
     newText.addEventListener('click', () =>{
-      let taskTable = document.getElementById('targetTable');
-      let row_num=taskTable.rows.length; 
-      while(row_num-1>0){
-        if (row_num>1) {
-          taskTable.deleteRow(row_num-1);
+      const taskTable = document.getElementById('targetTable');
+      let rowNum = taskTable.rows.length; 
+      while(rowNum-1>0){
+        if (rowNum>1) {
+          taskTable.deleteRow(rowNum-1);
         }
-        row_num-=1
+        rowNum -= 1
       }
       tasks.splice(id, 1)
       tasks.forEach(function(task) {
           if (id < task.id){
-            newId = task.id-1
-            tasks[newId].id=newId
-            appendTask(task)
+            newId = task.id-1;
+            tasks[newId].id=newId;
+            appendTask(task);
           }else{
-            appendTask(task)
+            appendTask(task);
           }
       });
-    })
+    });
   }
 
   function createBtn(newRow, status, id){
-    newCell=newRow.insertCell();
-    newText=document.createElement('button');
-    newText.textContent = status
+    newCell = newRow.insertCell();
+    newText = document.createElement('button');
+    newText.textContent = status;
     newCell.appendChild(newText);
-    newText.addEventListener('click', () =>{
+    newText.addEventListener('click', () => {
     })
   }
 
   function appendTask(task){
-    let taskTable=document.getElementById('targetTable');
-    let newRow=taskTable.insertRow();
-    createStatus(newRow, task.id)
-    createStatus(newRow, task.comment)
-    createBtn(newRow, task.status, task.id)
-    deleteBtn(newRow, '削除', task.id)
+    const taskTable = document.getElementById('targetTable');
+    const newRow = taskTable.insertRow();
+    createStatus(newRow, task.id);
+    createStatus(newRow, task.comment);
+    createBtn(newRow, task.status, task.id);
+    deleteBtn(newRow, '削除', task.id);
   }
   
-  createButton.addEventListener('click', () => {
-    const id=tasks.length
-    const comment=document.getElementById('task_input').value;
-    tasks.push({id, comment, status: '作業中'})
-    appendTask(tasks[id])
+  taskBtn.addEventListener('click', () => {
+    const id = tasks.length
+    const comment = document.getElementById('task_input').value;
+    tasks.push({id, comment, status: '作業中'});
+    appendTask(tasks[id]);
     document.sampleform.reset();
   });
 };
