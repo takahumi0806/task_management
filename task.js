eb3533899beaa52b1cf087ef5deeba660a5790ca
takahumi0806 <yabuta0439@hotmail.com>
2020-12-01 19:01:53 +0900

    Update task.js

--- task.js
+++ task.js
@@ -81,7 +81,7 @@ window.onload = () => {
     document.sampleform.reset();
   });
 
-  taskAll.addEventListener('click',() => {
+  taskAll.addEventListener('click', () => {
     deleteStatus();
     changeTask()
   });
